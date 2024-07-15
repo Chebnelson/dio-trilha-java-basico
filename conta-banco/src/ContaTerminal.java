028ee421c4a6a4f78b53d5a5d75a6536004a0911
Chebnelson <chebnelson@hotmail.com>
2024-07-14 22:45:19 -0300

    banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+
+        System.out.println("Por favor, digite o seu nome !");
+        double nome = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int agencia = scanner.nextInt();
+ 
+        System.out.println("Olá [0], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque");
+
+         
     }
 }
